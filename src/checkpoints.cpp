d4edc0107be6c8878681cac996469a4d57e025e8
John-Alan Simmons <simmons.johnalan@gmail.com>
2014-02-21 09:31:02 -0500

    Added checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -33,23 +33,18 @@ namespace Checkpoints
     //   (no blocks before with a timestamp after, none after with
     //    timestamp before)
     // + Contains no strange transactions
-    static MapCheckpoints mapCheckpoints;/* =
+    static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        (  1500, uint256("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967"))
-        (  4032, uint256("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846"))
-        (  8064, uint256("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70"))
-        ( 16128, uint256("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d"))
-        ( 23420, uint256("0xd80fdf9ca81afd0bd2b2a90ac3a9fe547da58f2530ec874e978fce0b5101b507"))
-        ( 50000, uint256("0x69dc37eb029b68f075a5012dcc0419c127672adb4f3a32882b2b3e71d07a20a6"))
-        ( 80000, uint256("0x4fcb7c02f676a300503f49c764a89955a8f920b46a8cbecb4867182ecdb2e90a"))
-        (120000, uint256("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131"))
-        (161500, uint256("0xdbe89880474f4bb4f75c227c77ba1cdc024991123b28b8418dbbf7798471ff43"))
-        (179620, uint256("0x2ad9c65c990ac00426d18e446e0fd7be2ffa69e9a7dcb28358a50b2b78b9f709"))
-        (240000, uint256("0x7140d1c4b4c2157ca217ee7636f24c9c73db39c4590c4e6eab2e3ea1555088aa"))
-        (383640, uint256("0x2b6809f094a9215bafc65eb3f110a35127a34be94b7d0590a096c3f126c6f364"))
-        (409004, uint256("0x487518d663d9f1fa08611d9395ad74d982b667fbdc0e77e9cf39b4f1355908a3"))
-        (456000, uint256("0xbf34f71cc6366cd487930d06be22f897e34ca6a40501ac7d401be32456372004"))
-        ;*/
+        (   10, uint256("0xc580b440fcd20fc7077b6847b1b6489c57daa9bfbcdad74aba3fdc82543bbe67"))
+        (  100, uint256("0xfcd62f75fc27365917f316039c83fe715771a129b467b9dbf012f802124a4a8a"))
+        (  512, uint256("0x383f102ea110b57962a64e71b1835629b13dfa1f8835b567c3c5aa651a318671"))
+        (  624, uint256("0x677fa22e3f538d07e1c52f305743e20780e730cd90ca4884fe0ef7d80c491eb5"))
+        (  728, uint256("0xa9ba127503d76ac96dd1cb97722bbd34f3f3cc471b704b479b5979fe6428ba6e"))
+        ( 1264, uint256("0x9f6f162efd939494f8155e2abfd7ef2c6be822c8c8430cc99fee1e20aca86366"))
+        ( 1400, uint256("0x74c12510ce76c400e782d6d41e7a17f23a27e54c9286d949067d69fcf2a6224a"))
+        ( 1800, uint256("0xd12efbd0c8f7f24115c080631a25217ee97294079d0baa17a4f4d4d65aa9c0aa"))
+        ( 2100, uint256("0xce8876923356af522fbd582e4c60a9fdb36f868084e0cb00f0ee6550272153a3"))
+        ;
     static const CCheckpointData data = {
         &mapCheckpoints,
         1383887110, // * UNIX timestamp of last checkpoint block
